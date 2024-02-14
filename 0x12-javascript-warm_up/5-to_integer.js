32e1d07fd8ba28f5b5023d13b9e7c096312fbae4
Rufai Abdulwasii Oladimeji <rufaiwasiu5@gmail.com>
2024-02-14 00:38:07 -0800

    this is just a debugging test

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-const argument = process.argv[2]
-
-// Check if the argument can be converted to an integer
-const intValue = +argument
-
-// Check if the conversion resulted in a valid number
-if (!isNaN(intValue) && Number.isInteger(intValue)) {
-  console.log(`My number: ${intValue}`);
-} else {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv[2]));
 }
